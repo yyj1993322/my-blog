4a4946f3c8aaadd5ed648d1a0d1e534a801df042
1577243473@qq.com <1577243473@qq.com>
2025-03-10 23:32:16 +0900

    删除不必要的文件，更新 PostCSS 配置，添加新的依赖，新增编码图片

---
+++ postcss.config.js
@@ -0,0 +1,15 @@
+// /** @type {import('postcss-load-config').Config} */
+// const config = {
+//   plugins: {
+//     tailwindcss: {},
+//   },
+// };
+
+// export default config;
+
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};
