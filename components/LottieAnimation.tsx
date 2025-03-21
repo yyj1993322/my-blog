dd85a873e5e7665415306fdbcc1bf51d1945d207
1577243473@qq.com <1577243473@qq.com>
2025-03-21 14:15:19 +0900

    新增 NextAnimation 组件，导入新的 Lottie 动画

--- components/LottieAnimation.tsx
+++ components/LottieAnimation.tsx
@@ -4,6 +4,7 @@ import dynamic from "next/dynamic";
 // import Lottie from "lottie-react";
 const Lottie = dynamic(() => import("lottie-react"), { ssr: false });
 import catAnimation from "../public/CatAnimation.json"; // 导入 JSON 动画
+import nextAnimation from "../public/nextAnimation.json"
 
 const LottieCat = () => {
   return <Lottie animationData={catAnimation} loop={true} />
@@ -11,6 +12,6 @@ const LottieCat = () => {
 
 export default LottieCat;
 
-export function LottieAnimation() {
-  return <Lottie animationData={catAnimation} loop={true} />;
+export function NextAnimation() {
+  return <Lottie animationData={nextAnimation} loop={true} />;
 };
