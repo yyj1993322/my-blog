47cd9f97c99b2089d78179c1b9b2acb311378bc1
1577243473@qq.com <1577243473@qq.com>
2025-03-24 21:31:22 +0900

    重构导入路径，使用别名简化 ThemeProvider 的引用

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import "@/styles/globals.css";
-import { ThemeProvider } from "components/ThemeProvider";
+import { ThemeProvider } from "@components/ThemeProvider";
 
 export default function RootLayout({
   children,
