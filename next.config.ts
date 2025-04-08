14584626074c4de203ddbfa2d096c7306282efbc
1577243473@qq.com <1577243473@qq.com>
2025-04-08 23:51:23 +0900

    优化 Next.js 配置，添加安全头部设置并调整脚本命令

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,40 @@
 import type { NextConfig } from "next";
 
+const securityHeaders = [
+  // {
+  //   key: 'Content-Security-Policy',
+  //   value:
+  //     "default-src 'self'; script-src 'self' 'unsafe-inline' https://unpkg.com https://*.lottiefiles.com; style-src 'self' 'unsafe-inline'; img-src * data: blob:; font-src *; connect-src *;",
+  // },
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff',
+  },
+  {
+    key: 'Referrer-Policy',
+    value: 'strict-origin-when-cross-origin',
+  },
+  {
+    key: 'X-Frame-Options',
+    value: 'DENY',
+  },
+  {
+    key: 'Strict-Transport-Security',
+    value: 'max-age=63072000; includeSubDomains; preload',
+  },
+]
+
 const nextConfig: NextConfig = {
-  output: 'export',
   reactStrictMode: true,
+
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: securityHeaders,
+      },
+    ]
+  },
 }
 
 export default nextConfig;
